2e58deb059ee07a1e8579e683a2f5ee8811e78e8
Yaroslav Furman <yaro330@gmail.com>
2019-06-08 19:43:43 +0300

    Revert "msm: vidc: fix KCFI errors"
    This reverts commit 7a945f91c539ef0dfd86447f08750a4dd7d38d4f.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/media/platform/msm/vidc/hfi_response_handler.c
+++ drivers/media/platform/msm/vidc/hfi_response_handler.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2019, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2018, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -366,10 +366,9 @@ static int hfi_process_session_error(u32 device_id,
 }
 
 static int hfi_process_event_notify(u32 device_id,
-		void *_pkt,
+		struct hfi_msg_event_notify_packet *pkt,
 		struct msm_vidc_cb_info *info)
 {
-	struct hfi_msg_event_notify_packet *pkt = _pkt;
 	dprintk(VIDC_DBG, "Received: EVENT_NOTIFY\n");
 
 	if (pkt->size < sizeof(struct hfi_msg_event_notify_packet)) {
@@ -408,10 +407,9 @@ static int hfi_process_event_notify(u32 device_id,
 }
 
 static int hfi_process_sys_init_done(u32 device_id,
-		void *_pkt,
+		struct hfi_msg_sys_init_done_packet *pkt,
 		struct msm_vidc_cb_info *info)
 {
-	struct hfi_msg_sys_init_done_packet *pkt = _pkt;
 	struct msm_vidc_cb_cmd_done cmd_done = {0};
 	enum vidc_status status = VIDC_ERR_NONE;
 
@@ -448,10 +446,9 @@ err_no_prop:
 }
 
 static int hfi_process_sys_rel_resource_done(u32 device_id,
-		void *_pkt,
+		struct hfi_msg_sys_release_resource_done_packet *pkt,
 		struct msm_vidc_cb_info *info)
 {
-	struct hfi_msg_sys_release_resource_done_packet *pkt = _pkt;
 	struct msm_vidc_cb_cmd_done cmd_done = {0};
 	enum vidc_status status = VIDC_ERR_NONE;
 	u32 pkt_size;
@@ -1255,10 +1252,9 @@ static void hfi_process_sess_get_prop_buf_req(
 }
 
 static int hfi_process_session_prop_info(u32 device_id,
-		void *_pkt,
+		struct hfi_msg_session_property_info_packet *pkt,
 		struct msm_vidc_cb_info *info)
 {
-	struct hfi_msg_session_property_info_packet *pkt = _pkt;
 	struct msm_vidc_cb_cmd_done cmd_done = {0};
 	struct buffer_requirements buff_req = { { {0} } };
 
@@ -1297,10 +1293,9 @@ static int hfi_process_session_prop_info(u32 device_id,
 }
 
 static int hfi_process_session_init_done(u32 device_id,
-		void *_pkt,
+		struct hfi_msg_sys_session_init_done_packet *pkt,
 		struct msm_vidc_cb_info *info)
 {
-	struct hfi_msg_sys_session_init_done_packet *pkt = _pkt;
 	struct msm_vidc_cb_cmd_done cmd_done = {0};
 	struct vidc_hal_session_init_done session_init_done = { {0} };
 
@@ -1325,10 +1320,9 @@ static int hfi_process_session_init_done(u32 device_id,
 }
 
 static int hfi_process_session_load_res_done(u32 device_id,
-		void *_pkt,
+		struct hfi_msg_session_load_resources_done_packet *pkt,
 		struct msm_vidc_cb_info *info)
 {
-	struct hfi_msg_session_load_resources_done_packet *pkt = _pkt;
 	struct msm_vidc_cb_cmd_done cmd_done = {0};
 
 	dprintk(VIDC_DBG, "RECEIVED: SESSION_LOAD_RESOURCES_DONE[%#x]\n",
@@ -1354,10 +1348,9 @@ static int hfi_process_session_load_res_done(u32 device_id,
 }
 
 static int hfi_process_session_flush_done(u32 device_id,
-		void *_pkt,
+		struct hfi_msg_session_flush_done_packet *pkt,
 		struct msm_vidc_cb_info *info)
 {
-	struct hfi_msg_session_flush_done_packet *pkt = _pkt;
 	struct msm_vidc_cb_cmd_done cmd_done = {0};
 
 	dprintk(VIDC_DBG, "RECEIVED: SESSION_FLUSH_DONE[%#x]\n",
@@ -1398,10 +1391,9 @@ static int hfi_process_session_flush_done(u32 device_id,
 }
 
 static int hfi_process_session_etb_done(u32 device_id,
-		void *_pkt,
+		struct hfi_msg_session_empty_buffer_done_packet *pkt,
 		struct msm_vidc_cb_info *info)
 {
-	struct hfi_msg_session_empty_buffer_done_packet *pkt = _pkt;
 	struct msm_vidc_cb_data_done data_done = {0};
 	struct hfi_picture_type *hfi_picture_type = NULL;
 
@@ -1453,10 +1445,9 @@ static int hfi_process_session_etb_done(u32 device_id,
 }
 
 static int hfi_process_session_ftb_done(
-		u32 device_id, void *_pkt,
+		u32 device_id, struct vidc_hal_msg_pkt_hdr *msg_hdr,
 		struct msm_vidc_cb_info *info)
 {
-	struct vidc_hal_msg_pkt_hdr *msg_hdr = _pkt;
 	struct msm_vidc_cb_data_done data_done = {0};
 	bool is_decoder = false, is_encoder = false;
 
@@ -1578,10 +1569,9 @@ static int hfi_process_session_ftb_done(
 }
 
 static int hfi_process_session_start_done(u32 device_id,
-		void *_pkt,
+		struct hfi_msg_session_start_done_packet *pkt,
 		struct msm_vidc_cb_info *info)
 {
-	struct hfi_msg_session_start_done_packet *pkt = _pkt;
 	struct msm_vidc_cb_cmd_done cmd_done = {0};
 
 	dprintk(VIDC_DBG, "RECEIVED: SESSION_START_DONE[%#x]\n",
@@ -1605,10 +1595,9 @@ static int hfi_process_session_start_done(u32 device_id,
 }
 
 static int hfi_process_session_stop_done(u32 device_id,
-		void *_pkt,
+		struct hfi_msg_session_stop_done_packet *pkt,
 		struct msm_vidc_cb_info *info)
 {
-	struct hfi_msg_session_stop_done_packet *pkt = _pkt;
 	struct msm_vidc_cb_cmd_done cmd_done = {0};
 
 	dprintk(VIDC_DBG, "RECEIVED: SESSION_STOP_DONE[%#x]\n",
@@ -1633,10 +1622,9 @@ static int hfi_process_session_stop_done(u32 device_id,
 }
 
 static int hfi_process_session_rel_res_done(u32 device_id,
-		void *_pkt,
+		struct hfi_msg_session_release_resources_done_packet *pkt,
 		struct msm_vidc_cb_info *info)
 {
-	struct hfi_msg_session_release_resources_done_packet *pkt = _pkt;
 	struct msm_vidc_cb_cmd_done cmd_done = {0};
 
 	dprintk(VIDC_DBG, "RECEIVED: SESSION_RELEASE_RESOURCES_DONE[%#x]\n",
@@ -1661,10 +1649,9 @@ static int hfi_process_session_rel_res_done(u32 device_id,
 }
 
 static int hfi_process_session_rel_buf_done(u32 device_id,
-		void *_pkt,
+		struct hfi_msg_session_release_buffers_done_packet *pkt,
 		struct msm_vidc_cb_info *info)
 {
-	struct hfi_msg_session_release_buffers_done_packet *pkt = _pkt;
 	struct msm_vidc_cb_cmd_done cmd_done = {0};
 
 	if (!pkt || pkt->size <
@@ -1691,10 +1678,9 @@ static int hfi_process_session_rel_buf_done(u32 device_id,
 }
 
 static int hfi_process_session_register_buffer_done(u32 device_id,
-		void *_pkt,
+		struct hfi_msg_session_register_buffers_done_packet *pkt,
 		struct msm_vidc_cb_info *info)
 {
-	struct hfi_msg_session_register_buffers_done_packet *pkt = _pkt;
 	struct msm_vidc_cb_cmd_done cmd_done = {0};
 
 	if (!pkt || pkt->size <
@@ -1719,10 +1705,9 @@ static int hfi_process_session_register_buffer_done(u32 device_id,
 }
 
 static int hfi_process_session_unregister_buffer_done(u32 device_id,
-		void *_pkt,
+		struct hfi_msg_session_unregister_buffers_done_packet *pkt,
 		struct msm_vidc_cb_info *info)
 {
-	struct hfi_msg_session_unregister_buffers_done_packet *pkt = _pkt;
 	struct msm_vidc_cb_cmd_done cmd_done = {0};
 
 	if (!pkt || pkt->size <
@@ -1747,10 +1732,9 @@ static int hfi_process_session_unregister_buffer_done(u32 device_id,
 }
 
 static int hfi_process_session_end_done(u32 device_id,
-		void *_pkt,
+		struct hfi_msg_sys_session_end_done_packet *pkt,
 		struct msm_vidc_cb_info *info)
 {
-	struct hfi_msg_sys_session_end_done_packet *pkt = _pkt;
 	struct msm_vidc_cb_cmd_done cmd_done = {0};
 
 	dprintk(VIDC_DBG, "RECEIVED: SESSION_END_DONE[%#x]\n", pkt->session_id);
@@ -1773,10 +1757,9 @@ static int hfi_process_session_end_done(u32 device_id,
 }
 
 static int hfi_process_session_abort_done(u32 device_id,
-	void *_pkt,
+	struct hfi_msg_sys_session_abort_done_packet *pkt,
 	struct msm_vidc_cb_info *info)
 {
-	struct hfi_msg_sys_session_abort_done_packet *pkt = _pkt;
 	struct msm_vidc_cb_cmd_done cmd_done = {0};
 
 	dprintk(VIDC_DBG, "RECEIVED: SESSION_ABORT_DONE[%#x]\n",
@@ -1842,10 +1825,9 @@ static void hfi_process_sys_get_prop_image_version(
 }
 
 static int hfi_process_sys_property_info(u32 device_id,
-		void *_pkt,
+		struct hfi_msg_sys_property_info_packet *pkt,
 		struct msm_vidc_cb_info *info)
 {
-	struct hfi_msg_sys_property_info_packet *pkt = _pkt;
 	if (!pkt) {
 		dprintk(VIDC_ERR, "%s: invalid param\n", __func__);
 		return -EINVAL;
@@ -1877,7 +1859,7 @@ static int hfi_process_sys_property_info(u32 device_id,
 }
 
 static int hfi_process_ignore(u32 device_id,
-		void *_pkt,
+		struct vidc_hal_msg_pkt_hdr *msg_hdr,
 		struct msm_vidc_cb_info *info)
 {
 	*info = (struct msm_vidc_cb_info) {
@@ -1966,6 +1948,5 @@ int hfi_process_msg_packet(u32 device_id, struct vidc_hal_msg_pkt_hdr *msg_hdr,
 		break;
 	}
 
-	return pkt_func ?
-		pkt_func(device_id, (void *)msg_hdr, info) : -ENOTSUPP;
+	return pkt_func ? pkt_func(device_id, msg_hdr, info) : -ENOTSUPP;
 }
