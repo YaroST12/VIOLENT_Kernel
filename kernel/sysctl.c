2f0dc64e5709b24d90a77f5356bb1a0e1fea3d16
Park Ju Hyung <qkrwngud825@gmail.com>
2019-06-24 21:37:21 +0900

    sysctl: promote sched_migration_cost_ns out of CONFIG_SCHED_DEBUG
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    [yaro]: adjusted for 4.14
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -410,6 +410,13 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &zero,
 		.extra2		= &two_million,
 	},
+	{
+		.procname	= "sched_migration_cost_ns",
+		.data		= &sysctl_sched_migration_cost,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec,
+	},
 #endif
 	{
 		.procname	= "sched_upmigrate",
@@ -476,13 +483,6 @@ static struct ctl_table kern_table[] = {
 		.proc_handler	= sched_proc_update_handler,
 		.extra1		= &min_sched_tunable_scaling,
 		.extra2		= &max_sched_tunable_scaling,
-	},
-	{
-		.procname	= "sched_migration_cost_ns",
-		.data		= &sysctl_sched_migration_cost,
-		.maxlen		= sizeof(unsigned int),
-		.mode		= 0644,
-		.proc_handler	= proc_dointvec,
 	},
 	{
 		.procname	= "sched_nr_migrate",
